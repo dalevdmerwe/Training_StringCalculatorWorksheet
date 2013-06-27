1e0ce15fca23845f2fa652ca5d6fd4a890594adc
Dale van der Merwe <dalevdmerwe@hotmail.com>
2013-06-27 11:15:35 +0200

    8 Works

--- StringCalculator/StringCalculator/Calculator.cs
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using StringCalculator;
 
@@ -11,8 +12,27 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-//            var myCalculator = new Calculator();
-//            myCalculator.Add("//;\n1;3");
+//          var myCalculator = new Calculator();
+//          myCalculator.Add("//;\n1;3");
+
+//          var pattern = @"\[(.*?)\]";
+//            var pattern = @"\[(.*?)\]";
+//            var query = "H1-receptor antagonist [HSA:3269] [PATH:hsa04080(3269)] [test]";
+//            var matches = Regex.Matches(query, pattern);
+//
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Groups[1]);
+//            }
+        }
+    }
+
+    public class NegativeValueException:Exception
+    {
+        public NegativeValueException()
+            : base("Negatives Not allowed")
+        {
+            
         }
     }
 
@@ -20,23 +40,60 @@ namespace StringCalculator
     {
         public int Add(string inputString)
         {
-            char [] delimeters = new char[5];
+            string [] delimeters = new string[50];
             if (inputString.Contains("//"))
             {
-                delimeters[0] = inputString.Substring(2, 1)[0];
-                inputString = inputString.Substring(3, inputString.Length - 3);
+                
+                var pattern = @"\[(.*?)\]";
+                var query = inputString;
+                var matches = Regex.Matches(query, pattern);
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    //Console.WriteLine(matches[i].Groups[1]);
+                    delimeters[i] = matches[i].Groups[1].ToString();
+                }
+
+                int firstemptyspace = 0;
+                for (int j = 0; j<delimeters.Length; j++)
+                {
+                    if (delimeters[j] == null)
+                    {
+                        firstemptyspace = j;
+                        break;
+                    }
+                }
+                int indexofnewlinechar = inputString.IndexOf('\n');
+                int lengthofdelimiter = indexofnewlinechar - 2;  //-2 for //
+
+                if (!inputString.Contains('[') && !inputString.Contains(']'))
+                {
+                    delimeters[firstemptyspace] = inputString.Substring(indexofnewlinechar - lengthofdelimiter, lengthofdelimiter);
+                }
+                inputString = inputString.Substring(indexofnewlinechar, inputString.Length - indexofnewlinechar);
             }
 
             if (inputString.Contains("-"))
             {
-                throw new Exception("Negatives Not allowed");
+                throw new NegativeValueException();
             }
 
-            delimeters[1] = '\n';
-            delimeters[2] = ',';
+            int firstemptyspace2 = 0;
+            for (int j = 0; j < delimeters.Length; j++)
+            {
+                if (delimeters[j] == null)
+                {
+                    firstemptyspace2 = j;
+                    break;
+                }
+            }
+
+
+            delimeters[firstemptyspace2] = '\n'.ToString();
+            delimeters[firstemptyspace2+1] = ','.ToString();
 
             var sumamount = 0;
-            var numbers = inputString.Split(delimeters);
+            var numbers = inputString.Split(delimeters,10,StringSplitOptions.None);
 
             
             foreach (var number in numbers)
@@ -100,7 +157,7 @@ namespace StringCalculator
         }
 
         [Test]
-        [ExpectedException(typeof(Exception))]
+        [ExpectedException(typeof(NegativeValueException))]
         public void AddingNegativesWillThrowException()
         {
             string negativeinput = "//&\n-5&7";
@@ -114,11 +171,19 @@ namespace StringCalculator
 
 
         [Test]
-        [ExpectedException("System.Exception : Negatives Not allowed")]
+        [ExpectedException(typeof(NegativeValueException))]
         public void NegativesThrowsException()
         {
             string negativeinput = "//&\n-5&7";
-            var sut = new Calculator().Add(negativeinput);
+            try
+            {
+                var sut = new Calculator().Add(negativeinput);
+            }
+            catch (NegativeValueException nve)
+            {
+                Assert.That(nve.Message, Is.EqualTo("Negatives Not allowed"));
+                throw;
+            }
         }
 
         [Test]
@@ -129,13 +194,23 @@ namespace StringCalculator
             Assert.That(sut, Is.EqualTo(2));
         }
 
-        [Test]
-        public void LongDelimitersSpecified()
+        [TestCase("//[***]\n1***2***3",6)]
+        [TestCase("//[///]\n1///2///3", 6)]
+        [TestCase("//[[[[]\n1[[[2[[[3", 6)]
+        public void LongDelimitersSpecified(string input, int result)
         {
-            string longDelimitersinputstring = "//[***]\n1***2***3";
-            var sut = new Calculator().Add(longDelimitersinputstring);
-            Assert.That(sut, Is.EqualTo(6));
+            //string longDelimitersinputstring = "//[***]\n1***2***3";
+            var sut = new Calculator().Add(input);
+            Assert.That(sut, Is.EqualTo(result));
         }
-        
+
+        [Test]
+        public void MultipletDelimeters()
+        {
+            string inputstring = "//[*][%]\n1*2%3";
+            var sut = new Calculator().Add(inputstring);
+            Assert.That(sut,Is.EqualTo(6));
+        }
+
     }
 }
